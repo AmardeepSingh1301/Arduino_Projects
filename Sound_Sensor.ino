373f1ebc84d43b5485287339a1a017f6a61811b8
Amardeep Singh <81309641+AmardeepSingh1301@users.noreply.github.com>
2021-04-07 14:54:11 +0530

    Add files via upload

---
+++ Sound_Sensor.ino
@@ -0,0 +1,21 @@
+int ledPin=8;
+int sensorPin=3;
+boolean val =0;
+
+void setup(){
+  pinMode(ledPin, OUTPUT);
+  pinMode(sensorPin, INPUT);
+  Serial.begin (9600);
+}
+  
+void loop (){
+  val =digitalRead(sensorPin);
+  Serial.println (val);
+  // when the sensor detects a signal above the threshold value, LED flashes
+  if (val==HIGH) {
+    digitalWrite(ledPin, LOW);
+  }
+  else {
+    digitalWrite(ledPin, HIGH);
+  }
+}
